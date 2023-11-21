c29cce2a9e3c56d5e43216ad88da8eef6928ad4c
Edson Camargo <dinhocmenezes@hotmail.com>
2023-11-20 22:00:16 -0300

    Added drag and drop feature to mobile and readme

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# Drag-and-drop
- Drag and drop with js
+# EcDragAndDrop
+
+![EcDragAndDrop](./assets/images/icon.jpg)
+
+**ec-drag-and-drop** is a straightforward project that features a static Kanban board with three columns: Todo, In Progress, and Done. The primary objective of this project is to provide a hands-on experience and demonstration of drag and drop functionality for managing tasks across different stages. It's important to note that this example is static, and additional lanes or tasks cannot be added. The project is developed using JavaScript, HTML, and Sass, serving as a valuable resource for learning and practicing drag and drop interactions.
+
+## Technology
+
+The project leverages the following technologies:
+
+- **JavaScript**: The primary programming language for building dynamic functionality.
+- **HTML**: Used for structuring the web page.
+- **SASS**: A preprocessor scripting language that is interpreted or compiled into CSS.
+
+## Features
+
+### Drag and Drop
+
+Experience the seamless drag and drop functionality, allowing you to move tasks across the different stages of the Kanban board. This feature is optimized for both desktop and mobile devices, providing a smooth user experience.
+
+### Limitations
+
+The Kanban board is static, and additional lanes or tasks cannot be added in this example.
+
+## How to Use
+
+Clone the repository: git clone https://github.com/edsoncamargo/EcDragAndDrop
+Open index.html in your preferred web browser.
+
+## Contribution
+
+Contributions to enhance the drag and drop functionality are welcomed! If you identify opportunities to improve the project or want to suggest new features, feel free to open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT LECINSE](./LICENSE).
+
+## Contact
+
+- Name: Edson Camargo Menezes
+- Email: ec-drag-and-drop@edsoncamargo.dev
+
+Explore the Kanban board with drag and drop functionality and enhance your understanding of web development 🚀
--- assets/js/drag.js
+++ assets/js/drag.js
@@ -1,75 +1,114 @@
 const dragStart = function (e) {
-    document.getElementById(e.target.id).classList.add('grabbing');
-}
-
-const dragEnd = function (e) {
-    const drop = document.querySelectorAll('.drop');
-
-    document.getElementById(e.target.id).classList.remove('grabbing');
-
-    drop.forEach(element => {
-        if ((e.clientX > element.getBoundingClientRect().x && e.clientX < element.getBoundingClientRect().x + element.clientWidth) &&
-            (e.clientY > element.getBoundingClientRect().y && e.clientY < element.getBoundingClientRect().y + element.clientHeight)) {
-
-            if (element.childElementCount === 0) {
-                element.appendChild(document.getElementById(e.target.id));
-            } else {
-                let isAddedBefore = false;
-                const childs = element.childNodes;
-
-                childs.forEach(child => {
-                    if (child.nodeName !== '#text') {
-                        if (isAddedBefore === false) {
-                            if ((e.clientX > child.getBoundingClientRect().x && e.clientX < child.getBoundingClientRect().x + child.clientWidth) &&
-                                (e.clientY > child.getBoundingClientRect().y && e.clientY < child.getBoundingClientRect().y + child.clientHeight)) {
-                                element.insertBefore(document.getElementById(e.target.id), child);
-                                isAddedBefore = true;
-                            } else {
-                                const oldElement = document.getElementById(e.target.id);
-                                const newElement = oldElement.cloneNode(true);
-                                document.getElementById(e.target.id).remove();
-                                element.appendChild(newElement);
-                            }
-                        }
-                    }
-                });
-            }
-        }
-
-        document.querySelectorAll('.drop > p').forEach(p => {
-            p.classList.remove('preview');
-        })
-
-        document.getElementById(element.id).classList.remove('over');
-    });
-}
+  document.getElementById(e.target.id).classList.add("grabbing");
+};
 
 const dragOver = function (e) {
-    const drop = document.querySelectorAll('.drop');
+  const drop = document.querySelectorAll(".drop");
+  const x =
+    e.touches && e.touches.length > 0 ? e.touches[0].clientX : e.clientX;
+  const y =
+    e.touches && e.touches.length > 0 ? e.touches[0].clientY : e.clientY;
 
-    drop.forEach(element => {
-        const childs = element.childNodes;
+  drop.forEach((element) => {
+    const childs = element.childNodes;
 
-        if ((e.clientX > element.getBoundingClientRect().x && e.clientX < element.getBoundingClientRect().x + element.clientWidth) &&
-            (e.clientY > element.getBoundingClientRect().y && e.clientY < element.getBoundingClientRect().y + element.clientHeight)) {
-            document.getElementById(element.id).classList.add('over');
+    if (
+      x > element.getBoundingClientRect().x &&
+      x < element.getBoundingClientRect().x + element.clientWidth &&
+      y > element.getBoundingClientRect().y &&
+      y < element.getBoundingClientRect().y + element.clientHeight
+    ) {
+      document.getElementById(element.id).classList.add("over");
 
-            childs.forEach(child => {
-                if (child.nodeName !== '#text') {
-                    if ((e.clientX > child.getBoundingClientRect().x && e.clientX < child.getBoundingClientRect().x + child.clientWidth) &&
-                        (e.clientY > child.getBoundingClientRect().y && e.clientY < child.getBoundingClientRect().y + child.clientHeight)) {
-                        child.classList.add('preview');
-                    } else {
-                        child.classList.remove('preview');
-                    }
-                }
-            });
-        } else {
-            document.getElementById(element.id).classList.remove('over');
+      childs.forEach((child) => {
+        if (child.nodeName !== "#text") {
+          if (
+            x > child.getBoundingClientRect().x &&
+            x < child.getBoundingClientRect().x + child.clientWidth &&
+            y > child.getBoundingClientRect().y &&
+            y < child.getBoundingClientRect().y + child.clientHeight
+          ) {
+            child.classList.add("preview");
+          } else {
+            child.classList.remove("preview");
+          }
         }
-    });
-}
+      });
+    } else {
+      document.getElementById(element.id).classList.remove("over");
+    }
+  });
+};
 
-document.addEventListener('dragstart', dragStart);
-document.addEventListener('dragend', dragEnd);
-document.addEventListener('dragover', dragOver);
+const dragEnd = function (e) {
+  const drop = document.querySelectorAll(".drop");
+  const x =
+    e.touches && e.changedTouches.length > 0
+      ? e.changedTouches[0].clientX
+      : e.clientX;
+  const y =
+    e.touches && e.changedTouches.length > 0
+      ? e.changedTouches[0].clientY
+      : e.clientY;
+
+  const hasGrabbing = document.querySelectorAll(".grabbing");
+  if (hasGrabbing && hasGrabbing.length > 0) {
+    hasGrabbing.forEach((e) => {
+      e.classList.remove("grabbing");
+    });
+  }
+
+  drop.forEach((element) => {
+    if (
+      x > element.getBoundingClientRect().x &&
+      x < element.getBoundingClientRect().x + element.clientWidth &&
+      y > element.getBoundingClientRect().y &&
+      y < element.getBoundingClientRect().y + element.clientHeight
+    ) {
+      if (element.childElementCount === 0) {
+        element.appendChild(document.getElementById(e.target.id));
+      } else {
+        let isAddedBefore = false;
+        const childs = element.childNodes;
+        childs.forEach((child) => {
+          if (child.nodeName !== "#text") {
+            if (isAddedBefore === false) {
+              if (
+                x > child.getBoundingClientRect().x &&
+                x < child.getBoundingClientRect().x + child.clientWidth &&
+                y > child.getBoundingClientRect().y &&
+                y < child.getBoundingClientRect().y + child.clientHeight
+              ) {
+                element.insertBefore(
+                  document.getElementById(e.target.id),
+                  child
+                );
+                isAddedBefore = true;
+              } else {
+                const oldElement = document.getElementById(e.target.id);
+                const newElement = oldElement.cloneNode(true);
+                document.getElementById(e.target.id).remove();
+                element.appendChild(newElement);
+              }
+            }
+          }
+        });
+      }
+    }
+    document.querySelectorAll(".drop > p").forEach((p) => {
+      p.classList.remove("preview");
+    });
+
+    document.getElementById(element.id).classList.remove("over");
+  });
+};
+
+// desktop
+document.addEventListener("dragstart", dragStart);
+document.addEventListener("dragover", dragOver);
+document.addEventListener("dragend", dragEnd);
+
+// mobile
+document.addEventListener("touchstart", dragStart);
+document.addEventListener("touchmove", dragOver);
+document.addEventListener("touchend", dragEnd);
